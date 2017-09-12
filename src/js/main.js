5f0dd16cb4741519f126db8818b39653eea03f02
Suna Jung <30277126+sunnyj2324@users.noreply.github.com>
2017-09-12 14:48:53 +0900

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 
 
-{function (){
+(function (){
   const viewer = document.getElementById('viewer');
   const btnGet = document.getElementById('get');
   const btnPost = document.getElementById('post');
@@ -8,33 +8,33 @@
   const btnDel = document.getElementById('delete');
   const inputUserid = document.getElementById('userid');
 
-  function render(data){
-    viewer.innerHTML = JSON.stringify(JSON.parse(data),null,2);
-  }
-
   btnGet.addEventListener('click', () => {
-    viewer.innerHTML ='';
-
     const userid = inputUserid.value;
     const url = userid ? '/users/${userid}' : '/users';
+    function get(url) {
+      // promise 생성과 반환
+      return new Promise(function () {
+        // XMLHttpRequest 객체의 생성
+        var req = new XMLHttpRequest();
+        // 비동기 방식으로 Request를 오픈한다
+        req.open('GET', `/users/${userid}`);
+        // Request를 전송한다
+        req.send();
 
-    server.get(url).then(render);
-  });
-
-  btnPost.addEventListener('click', () => {
-    viewer.innerHTML = '';
-
-    const userid = inputUserid.value;
-
-    if (!userid) return alert('userid를 입력하세요');
-
-    const password = document.getElementById('password').value;
-    const firstname = document.getElementById('fistname').value;
-    const lastname = document.getElementById('lastname').value;
-    
-    server.post('/users'),({userid,password,firstname,lastname})
-    .then(render);
-  });
-
-  
-});
+        // Event Handler
+        req.onreadystatechange = function () {
+          // 서버 응답 완료 && 정상 응답
+          if (req.readyState === XMLHttpRequest.DONE) {
+            if (req.status == 200) {
+              // resolve 메소드에 전달한 처리 결과는 then 메소드의 첫번째 콜백함수에서 취득 가능
+              const data = JSON.parse(req.responseText);
+              view.innerHTML = JSON.stringify(data);
+            } else {
+              // 서버의 응답이 정상이 아니면
+              // reject 메소드에 전달한 처리 결과는 then 메소드의 두번째 콜백함수에서 취득 가능
+              reject(req.statusText);
+            }
+          }
+        };
+      });
+    });
